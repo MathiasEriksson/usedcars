bdd274b977608e78eedb69094f315bfe149420aa
Mathias <mathias.eriksson@koti.fimnet.fi>
2020-01-08 02:53:28 +0200

    Added base for spider, scrapes first page and min info of cars in it

--- nettiauto/nettiauto/spiders/vaihtoauto.py
+++ nettiauto/nettiauto/spiders/vaihtoauto.py
@@ -4,8 +4,21 @@ import scrapy
 
 class VaihtoautoSpider(scrapy.Spider):
     name = 'vaihtoauto'
-    allowed_domains = ['https://www.nettiauto.com/en/vaihtoautot']
-    start_urls = ['http://https://www.nettiauto.com/en/vaihtoautot/']
+    start_urls = ['https://www.nettiauto.com/vaihtoautot?page=1']
 
     def parse(self, response):
-        pass
+
+        # Follow all links to cars in listingsData
+        # which are not on "upsell" promotion
+        cars_paths = '//div[@id="listingData"]/div[not(contains(@class, "upsellAd"))]/div/a[@class="childVifUrl tricky_link"]/@href'
+        for href in response.xpath(cars_paths):
+            yield response.follow(href, self.parse_car)
+        # add follow pagination links
+
+    def parse_car(self, response):
+        
+        yield {
+            'manufacturer': response.xpath('//*[@id="heightForSlogan"]/h1/a/span/text()').get().strip(),
+            'model': response.xpath('//*[@id="heightForSlogan"]/h1/a/span/span/text()').get().strip(),
+            'price': response.xpath('//*[@id="rightLogoWrap"]/span/span/span/text()').get().strip(),
+        }
